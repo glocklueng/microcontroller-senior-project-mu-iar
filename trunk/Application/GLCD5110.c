c9e41a5da8d960b29371aa85354812a608073523
pattaradanai@gmail.com <pattaradanai@gmail.com@7062d0eb-706a-9b74-2b00-4e491e068406>
2013-11-22 15:25:30 +0000

    Add on: - GLCD Nokia 5110 : can use the library (send String call lcdString(coloum, line, string); )

---
+++ trunk/Application/GLCD5110.c
@@ -0,0 +1,570 @@
+/*
+Project : Programmable Control of Airflow System for Maintaining Oxygen Saturation in Pre-term Infants
+Microcontroller : STM32F4 Discovery (STM32F407VG)
+File : GLCD5110.c
+*/
+//------------------------------------------------------------------------------
+#include "stm32f4xx.h"
+#include "main.h"
+#include "DAC_LTC1661.h"
+#include "stm32f4xx_it.h"
+#include "stm32f4xx_conf.h"
+#include "stm32f4xx_rcc.h"
+#include "stm32f4xx_usart.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_adc.h"
+#include "stm32f4xx_spi.h"
+#include "stm32f4xx_tim.h"
+#include "GLCD5110.h"
+//------------------------------------------------------------------------------
+/*--------------------------------------------------------------------------------------------------
+                                      Global Variables
+--------------------------------------------------------------------------------------------------*/
+static unsigned char  LcdCache [ LCD_CACHE_SIZE ];
+
+static int   LcdCacheIdx;
+static int   LoWaterMark;
+static int   HiWaterMark;
+static char  UpdateLcd;
+unsigned char glcd_ini=0;
+
+//------------------------------------------------------------------------------------------------//
+//---------------------------------- Function delay ----------------------------------------------//
+//------------------------------------------------------------------------------------------------//
+void delay_ms(volatile unsigned long ms)  // delay 1 ms per count @ Crystal 8.0 MHz and PLL9x or SYSCLK = 72 MHz
+{
+   volatile unsigned long i,j;
+	for (i = 0; i < ms; i++ )
+	for (j = 0; j < 5525; j++ );
+}
+
+void port_init()
+{
+  /*use SPI2 for Transfer data to PCD8544 (Nokia LCD)*/
+  GPIO_InitTypeDef GPIO_InitStruct;
+  SPI_InitTypeDef SPI_InitStruct;
+  
+  /*
+    PA8 = SPI1_NSS
+    PA5 = SPI1_CLK
+    PA7 = SPI1_MOIS (Master out Slave in)
+  Note : In the Master Mode and Tx Only , use MOSI and CLK 
+  */
+	
+//  RCC_APB1PeriphResetCmd(RCC_APB1Periph_SPI2, ENABLE);
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);  
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+    
+//  /* set GPIO init structure parameters values */
+//  GPIO_InitStruct.GPIO_Pin  = GPIO_Pin_13 | GPIO_Pin_15;                                      //Set for SCK Pin
+//  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+//  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+//  GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+//  GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+//  GPIO_Init(GPIOB, &GPIO_InitStruct);
+  
+  //For NSS Pin
+  GPIO_InitStruct.GPIO_Pin  = GPIO_Pin_1 ;                                     
+  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_25MHz;
+  GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOD, &GPIO_InitStruct);
+  
+  /*
+  	set output for NSS, RESET, D/C(Data/Command)
+                (3)NSS           |      PD1
+		(4)RESET	 |	PD6
+		(5)D/C		 |	PD3
+                (6)LED           |      PD2
+  */
+
+  /* set GPIO init structure parameters values */
+  GPIO_InitStruct.GPIO_Pin  = GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_6;             //Set for RESET, D/C Pin
+  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_25MHz;
+  GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOD, &GPIO_InitStruct);
+  
+//  //Enable Altinate Function for SPI Protoca
+//  GPIO_PinAFConfig(GPIOB,GPIO_PinSource13 ,GPIO_AF_SPI2);
+//  GPIO_PinAFConfig(GPIOB,GPIO_PinSource15 ,GPIO_AF_SPI2);
+  
+  GPIO_SetBits(GPIOD, GPIO_Pin_1);                                              // NSS Pin is High
+	
+//  //Config SPI
+//  //APB1 Prescale : 4, 168/4= 42MHz, So SPI BaudRate = 42/16 = 2.625 MHz
+//  SPI_InitStruct.SPI_Direction = SPI_Direction_1Line_Tx;			// Tx Only
+//  SPI_InitStruct.SPI_Mode = SPI_Mode_Master;
+//  SPI_InitStruct.SPI_DataSize = SPI_DataSize_8b;				//Data size is 8 bits
+//  SPI_InitStruct.SPI_CPOL = SPI_CPOL_Low;
+//  SPI_InitStruct.SPI_CPHA = SPI_CPHA_1Edge;
+//  SPI_InitStruct.SPI_NSS = SPI_NSS_Soft | SPI_NSSInternalSoft_Set;
+//  SPI_InitStruct.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_16;              
+//  SPI_InitStruct.SPI_FirstBit = SPI_FirstBit_MSB;
+//  SPI_Init(SPI2, &SPI_InitStruct);
+//  
+//  SPI_NSSInternalSoftwareConfig(SPI2, SPI_NSSInternalSoft_Set);
+//  
+//  //Enable select output
+//  SPI_SSOutputCmd(SPI2, ENABLE);
+//  
+//  //Enable SPI2
+//  SPI_Cmd(SPI2, ENABLE);
+  
+  // Reconfig Size Data
+  SPI_DataSizeConfig(SPI2, SPI_DataSize_8b);
+  //Reconfig
+  
+  //Set LED On
+  GPIO_SetBits(GPIOD, GPIO_Pin_2);
+}
+
+//------------------------------------------------------------------------------
+void lcdInit ( void )
+{
+   port_init();
+   delay_ms(10); // Ensure delay for initial
+   GPIO_ResetBits(GPIOD, GPIO_Pin_6);						//PD6 is RESET Pin
+   GPIO_SetBits(GPIOD, GPIO_Pin_6);
+   
+   GPIO_SetBits(GPIOD, GPIO_Pin_1);						//NSS Pin is High
+   lcdSend( 0x21, LCD_CMD );  // LCD Extended Commands.
+   lcdSend( 0xC8, LCD_CMD );  // Set LCD Vop (Contrast).
+   lcdSend( 0x06, LCD_CMD );  // Set Temp coefficent.
+   lcdSend( 0x13, LCD_CMD );  // LCD bias mode 1:48.
+   lcdSend( 0x20, LCD_CMD );  // LCD Standard Commands, Horizontal addressing mode.
+   lcdSend( 0x0C, LCD_CMD );  // LCD in normal mode.
+    
+   //  Reset watermark pointers.
+   LoWaterMark = LCD_CACHE_SIZE;
+   HiWaterMark = 0;
+   lcdClear();
+   lcdUpdate();
+}
+
+/*--------------------------------------------------------------------------------------------------
+
+  Name         :  lcdClear
+
+  Description  :  Clears the display. lcdUpdate must be called next.
+
+  Argument(s)  :  None.
+
+  Return value :  None.
+
+--------------------------------------------------------------------------------------------------*/
+void lcdClear ( void )
+{
+    int i;
+
+    for ( i = 0; i < LCD_CACHE_SIZE; i++ )
+    {
+        LcdCache[i] = 0x00;
+    }
+
+    //  Reset watermark pointers.
+    LoWaterMark = 0;
+    HiWaterMark = LCD_CACHE_SIZE - 1;
+
+    UpdateLcd = TRUE;
+}
+
+
+/*--------------------------------------------------------------------------------------------------
+
+  Name         :  lcdGotoXY
+
+  Description  :  Sets cursor location to xy location corresponding to basic font size.
+
+  Argument(s)  :  x, y -> Coordinate for new cursor position. Range: 1,1 .. 14,6
+
+  Return value :  None.
+
+--------------------------------------------------------------------------------------------------*/
+void lcdGotoXY ( unsigned char x, unsigned char y )
+{
+    configGlcd();
+    LcdCacheIdx = (x - 1) * 6 + (y - 1) * 84;
+}
+
+void configGlcd()
+{
+    if(glcd_ini==0)
+    {
+        glcd_ini=1;
+        lcdInit();
+        lcdClear();
+    }
+}
+/*--------------------------------------------------------------------------------------------------
+
+  Name         :  lcdSend
+
+  Description  :  Sends data to display controller.
+
+  Argument(s)  :  data -> Data to be sent
+                  cd   -> Command or data (see/use enum)
+  Note:
+   PD7 		: 	NSS Pin
+   PD5 		:	D/C Pin (Data/Command)
+   PD6 		: 	
+
+  Return value :  None.
+
+--------------------------------------------------------------------------------------------------*/
+void lcdSend ( unsigned char Data_Send, LcdCmdData cd )
+{
+    /*
+        PD5 is D/C Pin 
+        D/C Pin is High, Select Send Data
+        D/C Pin is Low, Select Send Command
+    */
+  
+    uint16_t i;
+    if ( cd == LCD_DATA )
+    {
+      //Select sent to Data Register
+      GPIO_SetBits(GPIOD, GPIO_Pin_3);
+    }
+    else 
+    {
+      //Select sent command
+      GPIO_ResetBits(GPIOD, GPIO_Pin_3);
+    }
+    
+    GPIO_ResetBits(GPIOD, GPIO_Pin_1);						// NSS Pin is Low
+    
+    while (SPI_I2S_GetFlagStatus(SPI2, SPI_I2S_FLAG_BSY) == RESET)
+    {
+      while (SPI_I2S_GetFlagStatus(SPI2, SPI_I2S_FLAG_TXE) == SET)
+      {
+        SPI_I2S_SendData(SPI2, Data_Send);
+      }
+    }
+    for(i=0;i<750;i++);
+    GPIO_SetBits(GPIOD, GPIO_Pin_1);						// NSS Pin is High
+}
+
+/*--------------------------------------------------------------------------------------------------
+
+  Name         :  lcdChar	
+
+  Description  :  Displays a character at current cursor location and increment cursor location.
+
+  Argument(s)  :  size -> Font size. See enum.
+                  ch   -> Character to write.
+
+  Return value :  None.
+
+--------------------------------------------------------------------------------------------------*/
+void lcdChar (char ch )
+{
+    unsigned char i;
+    configGlcd();
+    if ( LcdCacheIdx < LoWaterMark )
+    {
+        //  Update low marker.
+        LoWaterMark = LcdCacheIdx;
+    }
+
+    if ( (ch < 0x20) || (ch > 0x7b) )
+    {
+        //  Convert to a printable character.
+        ch = 92;
+    }
+
+    for ( i = 0; i < 5; i++ )
+    {
+        LcdCache[LcdCacheIdx++] = FontLookup[ch - 32][i] << 1;
+    }
+    if ( LcdCacheIdx > HiWaterMark )
+    {
+        //  Update high marker.
+        HiWaterMark = LcdCacheIdx;
+    }
+
+    //  Horizontal gap between characters.
+    LcdCache[LcdCacheIdx++] = 0x00;
+}
+
+/*--------------------------------------------------------------------------------------------------
+
+  Name         :  lcdString
+
+  Description  :  Displays a character at current cursor location and increment cursor location
+                  according to font size.
+
+  Argument(s)  :  size    -> Font size. See enum.
+                  dataPtr -> Pointer to null terminated ASCII string to display.
+
+  Return value :  None.
+
+--------------------------------------------------------------------------------------------------*/
+void lcdString (unsigned char _x,unsigned char _y,char *dataPtr)
+{
+    configGlcd();
+    lcdGotoXY(_x,_y);
+    while (*dataPtr)
+    {
+        lcdChar(*dataPtr++);
+    }
+}
+/*--------------------------------------------------------------------------------------------------
+
+  Name         :  lcdPixel
+
+  Description  :  Displays a pixel at given absolute (x, y) location.
+
+  Argument(s)  :  x, y -> Absolute pixel coordinates
+                  mode -> Off, On or Xor. See enum.
+
+  Return value :  None.
+
+--------------------------------------------------------------------------------------------------*/
+void lcdPixel(unsigned char x, unsigned char y, unsigned char mode)
+{
+    unsigned int  index;
+    unsigned char  offset;
+    unsigned char  data;
+
+    configGlcd();
+    if ( x > LCD_X_RES ) return;
+    if ( y > LCD_Y_RES ) return;
+
+    index = ((y / 8) * 84) + x;
+    offset  = y - ((y / 8) * 8);
+
+    data = LcdCache[index];
+
+    if ( mode == PIXEL_OFF )
+	{
+        data &= (~(0x01 << offset));
+    }
+    else if ( mode == PIXEL_ON )
+    {
+        data |= (0x01 << offset);
+    }
+    else if ( mode  == PIXEL_XOR )
+    {
+        data ^= (0x01 << offset);
+    }
+
+    LcdCache[index] = data;
+
+    if ( index < LoWaterMark )
+    {
+        //  Update low marker.
+        LoWaterMark = index;
+    }
+
+    if ( index > HiWaterMark )
+    {
+        //  Update high marker.
+        HiWaterMark = index;
+    }
+}
+
+/*--------------------------------------------------------------------------------------------------
+
+  Name         :  lcdLine
+
+  Description  :  Draws a line between two points on the display.
+
+  Argument(s)  :  x1, y1 -> Absolute pixel coordinates for line origin.
+                  x2, y2 -> Absolute pixel coordinates for line end.
+                  mode   -> Off, On or Xor. See enum.
+
+  Return value :  None.
+
+--------------------------------------------------------------------------------------------------*/
+void lcdLine ( unsigned char x1, unsigned char y1, unsigned char x2, unsigned char y2, unsigned char mode )
+{
+    int dx, dy, stepx, stepy, fraction;
+    configGlcd();
+    dy = y2 - y1;
+    dx = x2 - x1;
+
+    if ( dy < 0 )
+    {
+        dy    = -dy;
+        stepy = -1;
+    }
+    else
+    {
+        stepy = 1;
+    }
+
+    if ( dx < 0 )
+    {
+        dx    = -dx;
+        stepx = -1;
+    }
+    else
+    {
+        stepx = 1;
+    }
+
+    dx <<= 1;
+    dy <<= 1;
+
+    lcdPixel( x1, y1, mode );
+
+    if ( dx > dy )
+    {
+        fraction = dy - (dx >> 1);
+        while ( x1 != x2 )
+        {
+            if ( fraction >= 0 )
+            {
+                y1 += stepy;
+                fraction -= dx;
+            }
+            x1 += stepx;
+            fraction += dy;
+            lcdPixel( x1, y1, mode );
+        }
+    }
+    else
+    {
+        fraction = dx - (dy >> 1);
+        while ( y1 != y2 )
+        {
+            if ( fraction >= 0 )
+            {
+                x1 += stepx;
+                fraction -= dy;
+            }
+            y1 += stepy;
+            fraction += dx;
+            lcdPixel( x1, y1, mode );
+        }
+    }
+
+    UpdateLcd = TRUE;
+}
+
+/*--------------------------------------------------------------------------------------------------
+
+  Name         :  lcdUpdate
+
+  Description  :  Copies the LCD cache into the device RAM.
+
+  Argument(s)  :  None.
+
+  Return value :  None.
+
+--------------------------------------------------------------------------------------------------*/
+void lcdUpdate ( void )
+{
+    int i;
+
+    if ( LoWaterMark < 0 )
+        LoWaterMark = 0;
+    else if ( LoWaterMark >= LCD_CACHE_SIZE )
+        LoWaterMark = LCD_CACHE_SIZE - 1;
+
+    if ( HiWaterMark < 0 )
+        HiWaterMark = 0;
+    else if ( HiWaterMark >= LCD_CACHE_SIZE )
+        HiWaterMark = LCD_CACHE_SIZE - 1;
+
+    //  Set base address according to LoWaterMark.
+    lcdSend( 0x80 | (LoWaterMark % LCD_X_RES), LCD_CMD );
+    lcdSend( 0x40 | (LoWaterMark / LCD_X_RES), LCD_CMD );
+
+    //  Serialize the video buffer.
+    for ( i = LoWaterMark; i <= HiWaterMark; i++ )
+    {
+        lcdSend( LcdCache[i], LCD_DATA );
+    }
+
+    //  Reset watermark pointers.
+    LoWaterMark = LCD_CACHE_SIZE - 1;
+    HiWaterMark = 0;
+
+    UpdateLcd = FALSE;
+}
+/*--------------------------------------------------------------------------------------------------
+// Purpose:       Draw a rectangle on a graphic LCD
+// Inputs:        (x1, y1) - the start coordinate
+//                (x2, y2) - the end coordinate
+//                fill  - YES or NO
+//                color - ON or OFF
+// Dependencies:  glcd_pixel(), glcd_line()
+--------------------------------------------------------------------------------------------------*/
+void lcdRect(	unsigned char x1, 
+				unsigned char y1, 
+				unsigned char x2, 
+				unsigned char y2, 
+				unsigned char fill, 
+				unsigned char color)
+{
+   if(fill)
+   {
+      unsigned char  i, xmin, xmax, ymin, ymax;
+      
+      if(x1 < x2)     //  Find x min and max
+      {
+         xmin = x1;
+         xmax = x2;
+      }
+      else
+      {
+         xmin = x2;
+         xmax = x1;
+      }
+
+      if(y1 < y2)                            // Find the y min and max
+      {
+         ymin = y1;
+         ymax = y2;
+      }
+      else
+      {
+         ymin = y2;
+         ymax = y1;
+      }
+
+      for(; xmin <= xmax; ++xmin)
+      {
+         for(i=ymin; i<=ymax; ++i)
+         {
+            lcdPixel(xmin, i, color);
+         }
+      }
+   }
+   else
+   {
+      lcdLine(x1, y1, x2, y1, color);      // Draw the 4 sides
+      lcdLine(x1, y2, x2, y2, color);
+      lcdLine(x1, y1, x1, y2, color);
+      lcdLine(x2, y1, x2, y2, color);
+   }
+}
+//------------------------------------------------------------------------------------------------//
+//---------------------------------- Function for draw Progress Bar ------------------------------//
+//------------------------------------------------------------------------------------------------//
+void lcdProgBar(    unsigned char _x, 
+				    unsigned char _y, 
+				    unsigned char width, 
+				    unsigned char high, 
+				    unsigned char percentage)
+{
+    lcdRect(_x,_y,_x+width,_y+high,0,PIXEL_ON);   // Draw Beyon
+    lcdRect(_x+1,_y+1,_x+width-1,_y+high-1,1,PIXEL_OFF);    // Clear fill
+    lcdRect(_x,_y,_x+((percentage*width)/100),_y+high,1,PIXEL_ON); // Paint color @ percentage
+
+}
+void lcdBackLight(char set)
+{
+  if(set == SET)
+  {
+    GPIO_SetBits(GPIOD,GPIO_Pin_2);                                             // Control LED back light on
+  }
+  else if (set ==  RESET)
+  {
+    GPIO_ResetBits(GPIOD, GPIO_Pin_2);                                          // Control LED back light off
+  }
+}
